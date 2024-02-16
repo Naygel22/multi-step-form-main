be6bbbd7f988a1be124db2473d48a09c07ac56b5
Naygel22 <piotrkozlowski14@gmail.com>
2024-02-16 15:11:18 +0100

    added selecting items

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const inputTexts = document.querySelectorAll('.inputText');
 const nextStepButton = document.querySelector('.nextStepButton');
 const errorTexts = document.querySelectorAll('.errorText');
 let currentStep = 1;
-const items = document.querySelector('.items');
+const items = document.querySelectorAll('.item'); //changed
 const toggleSwitch = document.querySelector('.toggleSwitch');
 const toggleNameMonthly = document.querySelector('#toggleNameMonthly');
 const toggleNameYearly = document.querySelector('#toggleNameYearly');
@@ -12,75 +12,67 @@ const priceAdvanced = document.querySelector('.priceAdvanced');
 const pricePro = document.querySelector('.pricePro');
 const bonuses = document.querySelectorAll('.bonus');
 
+const stepPage1 = document.querySelector('.stepPage1');
+const stepPage2 = document.querySelector('.stepPage2');
+const stepPage3 = document.querySelector('.stepPage3');
+
 nextStepButton.addEventListener('click', () => {
-    checkInputFields();
+    currentStep = checkPage();
+    if(currentStep === 1) {
+        stepPage1.classList.remove('hidden');
+        stepPage2.classList.add('hidden');
+        stepPage3.classList.add('hidden');
+        checkInputFields();
+        console.log(currentStep);
+    }
+    if(currentStep === 2) {
+        currentStep++;
+        stepPage1.classList.add('hidden');
+        stepPage2.classList.remove('hidden');
+        stepPage3.classList.add('hidden');
+        console.log(currentStep);
+    }
+    if(currentStep === 3) {
+        stepPage1.classList.add('hidden');
+        stepPage2.classList.add('hidden');
+        stepPage3.classList.remove('hidden');
+        
+    }
 });
 
+
+function checkPage() {
+    if (!stepPage1.classList.contains('hidden')) {
+        return 1;
+    }
+    if (!stepPage2.classList.contains('hidden')) {
+        return 2;
+    }
+    if (!stepPage3.classList.contains('hidden')) {
+        return 3;
+    }
+}
+
 function checkInputFields() {
+    let bracketsAreFilled = true;
+
     inputTexts.forEach((element, index) => {
         const errorText = errorTexts[index];
 
         if (element.value === '') {
             element.classList.add('errorInput');
             errorText.classList.remove('hidden');
+            bracketsAreFilled = false;
         } else {
             element.classList.remove('errorInput');
             errorText.classList.add('hidden');
         }
     });
+    if(bracketsAreFilled) {
+        currentStep++;
+        console.log(currentStep)
+    }
 }
-
-// const planItems = {
-//     "Arcade": {
-//         image: "assets/images/icon-arcade.svg",
-//         title: "Arcade",
-//         priceMonthly: "$9/mo",
-//         priceYearly: "$90/yr",
-//         bonus: "2 months free"
-//     },
-//     "Advanced": {
-//         image: "assets/images/icon-advanced.svg",
-//         title: "Advanced",
-//         priceMonthly: "$12/mo",
-//         priceYearly: "$120/yr",
-//         bonus: "2 months free"
-//     },
-//     "Pro": {
-//         image: "assets/images/icon-pro.svg",
-//         title: "Pro",
-//         priceMonthly: "$15/mo",
-//         priceYearly: "$150/yr",
-//         bonus: "2 months free"
-//     }
-// }
-
-// function createPlanItems() {
-//     for (const key in planItems) {
-//         const item = document.createElement('div');
-//         item.classList.add('item');
-//         items.appendChild(item);
-
-//         const img = document.createElement('img');
-//         img.src = planItems[key].image;
-//         item.appendChild(img);
-
-//         const title = document.createElement('div');
-//         title.textContent = planItems[key].title;
-//         title.classList.add('itemTitle')
-//         item.appendChild(title);
-
-//         const price = document.createElement('div');
-//         price.textContent = planItems[key].priceMonthly;
-//         item.appendChild(price);
-//         price.classList.add('price');
-
-       
-//     }
-// }
-
-// createPlanItems();
-
-
     toggleSwitch.addEventListener('change', () => {
         if (toggleSwitch.checked) {
             toggleNameMonthly.classList.remove("toggleNameActivated");
@@ -99,4 +91,35 @@ function checkInputFields() {
             bonuses.forEach(bonus => bonus.classList.add('hidden'));
         }
     });
+
+   // step 2
+items.forEach(item => {
+    item.addEventListener('click', () => {
+        // Odznacz pozostałe elementy
+        items.forEach(otherItem => {
+            if (otherItem !== item) {
+                otherItem.classList.remove('selectedItem');
+            }
+        });
+
+        // Zaznacz/Wyłącz aktualny element
+        item.classList.toggle('selectedItem');
+    });
+});
+
+    
+
+
+    //step 3
+    const barItems = document.querySelectorAll('.barItem');
+
+    barItems.forEach(barItem => {
+    const checkbox = barItem.querySelector('.checkbox');
+
+    barItem.addEventListener('click', () => {
+        barItem.classList.toggle('selectedItem');
+        checkbox.checked = !checkbox.checked;
+    });
+});
+
 
